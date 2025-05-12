952cb43ea0639cf856c8812489adf7a61163158a
andreadellacorte <andrea@dellacorte.me>
2025-05-12 18:51:40 +0000

    nit: logging

--- src/urbansound8k-classifier/train_onefold.py
+++ src/urbansound8k-classifier/train_onefold.py
@@ -227,6 +227,7 @@ if __name__ == "__main__":
             import traceback
             traceback.print_exc()
         finally:
+            print("Cleaning up...")
             wandb.finish()
             print("Example complete")
     
@@ -239,3 +240,4 @@ if __name__ == "__main__":
     finally:
         print("Cleaning up...")
         wandb.finish()
+        print("Training complete")
